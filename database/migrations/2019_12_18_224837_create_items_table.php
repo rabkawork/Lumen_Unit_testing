939b892b3560bad67996f8bc7f91670f98cb6cda
unknown <rabkawork@gmail.com>
2019-12-19 05:49:43 +0700

    update migrations

--- database/migrations/2019_12_18_224837_create_items_table.php
+++ database/migrations/2019_12_18_224837_create_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChecklistsTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateChecklistsTable extends Migration
      */
     public function up()
     {
-        Schema::create('checklists', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('type',40);
-            $table->string('attr_object_domain');
-            $table->string('attr_object_id');
+            // $table->string('attr_object_domain');
+            // $table->string('attr_object_id');
             $table->text('attr_description');
             $table->boolean('attr_is_complete');
             $table->dateTime('attr_completed_at', 0);  
@@ -26,6 +26,7 @@ class CreateChecklistsTable extends Migration
             $table->integer('attr_due_interval');
             $table->string('attr_due_unit');
             $table->integer('attr_urgency');
+            $table->integer('checklist_id');
             $table->integer('template_id');
             $table->integer('pos');
             $table->timestamps();
@@ -39,6 +40,6 @@ class CreateChecklistsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('checklists');
+        Schema::dropIfExists('items');
     }
 }
