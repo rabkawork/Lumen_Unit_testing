b6e9619351d902f5dee700e1f1008aafbe09907f
unknown <rabkawork@gmail.com>
2019-12-18 19:56:11 +0700

    create users table => migration

--- database/migrations/2019_12_18_134656_create_users_table.php
+++ database/migrations/2019_12_18_134656_create_users_table.php
@@ -15,6 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique()->notNullable();
+            $table->string('password');
             $table->timestamps();
         });
     }
